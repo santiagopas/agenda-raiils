912f44ef46cc7162ef97b0d69dc03b0d9c81b2c6
santiagopas <santiagopasciullo@gmail.com>
2021-08-23 00:26:01 -0300

    configuracion de devise y estilos

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_155641) do
+ActiveRecord::Schema.define(version: 2021_08_22_165758) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
     t.string "phone"
-    t.string "instagram"
+    t.string "instagrargm"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
